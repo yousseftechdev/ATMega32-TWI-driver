bfc5485af37726899d562dbb698f7522d9cc78b2
yousseftechdev <youssef200895@gmail.com>
2026-07-22 00:14:08 +0300

    Finished implementing all major functions and the interrupt handler

--- TWI/TWI_interface.h
+++ TWI/TWI_interface.h
@@ -7,21 +7,144 @@
 #ifndef TWI_INTERFACE_H_
 #define TWI_INTERFACE_H_
 
+/* Include stdbool to use bool types */
+#include <stdbool.h>
+
 /* Standard Data Type Definitions (Matching System Architecture) */
-typedef unsigned char       u8;
-typedef unsigned short int  u16;
-typedef unsigned long int   u32;
+typedef unsigned char u8;
+typedef unsigned short int u16;
+typedef unsigned long int u32;
 
 /* Clock Prescaler Configurations (Hardware Divider for the TWI Bit Rate Generator) */
-#define TWI_PRESCALER_1        0b00   /* Divide system clock by 1   */
-#define TWI_PRESCALER_4        0b01   /* Divide system clock by 4   */
-#define TWI_PRESCALER_16       0b10   /* Divide system clock by 16  */
-#define TWI_PRESCALER_64       0b11   /* Divide system clock by 64  */
+#define TWI_PRESCALER_1 0  /* Divide system clock by 1   */
+#define TWI_PRESCALER_4 1  /* Divide system clock by 4   */
+#define TWI_PRESCALER_16 2 /* Divide system clock by 16  */
+#define TWI_PRESCALER_64 3 /* Divide system clock by 64  */
+
+/* Enable/Disable Interrupt on TWINT flag set */
+#define TWI_ENABLE_INTERRUPT true
+#define TWI_DISABLE_INTERRUPT false
+
+/* Read/Write bits */
+#define TWI_READ true
+#define TWI_WRITE false
+
+/* Acknowledge enable bit */
+#define TWI_ACKNOWLEDGE_ENABLE 1
+#define TWI_ACKNOWLEDGE_DISABLE 0
+
+/* Disable auto frequency calculation alogrithm flag, pass as first argument of TWI_vInit()
+   to disable auto Bit Rate Generator byte and prescaler calculation algorithm */
+#define TWI_NO_AUTO_FREQUENCY 0
 
 /* ============================================================================= */
 /* FUNCTION PROTOTYPES                                                           */
 /* ============================================================================= */
 
-void TWI_vInit();
+/**
+ * @brief                     Powers on the TWI module, sets SCL frequency, enables TWI interrupt.
+ * @param u32Freq             Desired SCL frequency
+ * @param boolInterruptEnable Whether to enable interrupts on TWINT flag set
+ */
+void TWI_vInit(u32 u32Freq, bool boolInterruptEnable);
+
+/**
+ * @brief         Sets frequency, runs in initialization.
+ * @param u32Freq Desired SCL frequency
+ * @note          Don't run this on its own, it's a helper function for in-driver logic.
+ */
+void TWI_vSetFrequency(u32 u32Freq);
+
+/**
+* @brief Enables TWI event based interrupts.
+* @note  Runs automatically in initialization function
+         when true is passed in boolInterruptEnable, no need to run manually.
+*/
+void TWI_vEnableInterrupt();
+
+/**
+ * @brief               Sets the bit rate generator bits in TWBR.
+ * @param u8BitRateByte The 8 bits to be written to the TWBR register
+ */
+void TWI_vWriteBitRateRegister(u8 u8BitRateByte);
+
+/**
+ * @brief             Writes the prescaler bits in TWSR.
+ * @param u8Prescaler The prescaler value
+ */
+void TWI_vWritePrescaler(u8 u8Prescaler);
+
+/**
+ * @brief                 Writes the ATMega's own slave address to TWAR.
+ * @param u8Address       The desired address
+ * @param boolGeneralCall General call enable
+ */
+void TWI_vSetOwnSlaveAddress(u8 u8Address, bool boolGeneralCall);
+
+/**
+ * @brief                       Allows the ATMega to to toggle whether to 
+ *                              send acknowledge bit when own address is called.
+ * @param boolAcknowledgeEnable Decide whether to send acknowledge pulses when address is detected
+ */
+void TWI_vAcknowledgeOwnAddress(bool boolAcknowledgeEnable);
+
+/**
+ * @brief Sends a start bit.
+ */
+void TWI_vStartTransmission(void);
+
+/**
+ * @brief Sends a stop bit.
+ */
+void TWI_vEndTransmission(void);
+
+/**
+ * @brief               Sends SLA+R/W (Slave address and write bit) call.
+ * @param u8Address     7-bit address for desired slave
+ * @param boolDirection Bit which decides whether the master wants to read or write to the slave
+ */
+void TWI_vSendSlaveCall(u8 u8Address, bool boolDirectionBit);
+
+/**
+ * @brief            Writes byte to TWDR then sends it by setting TWINT.
+ * @param u8DataByte Byte to be written and sent
+ */
+void TWI_vSendDataByte(u8 u8DataByte);
+
+/**
+ * @brief  Reads recieved byte from TWDR.
+ * @return Recieved byte from TWDR
+ */
+u8 TWI_u8ReadDataByte(void);
+
+/**
+ * @brief           Actually kicks off the transmission and sets all the necessary flags at the beginning and puts the slave in wriite mode.
+ * @param u8Address Target slave address
+ * @param pData     Array pointer for the bytes to be transmitted
+ * @param u8Size    Number of bytes to send
+ * @return true if busy, false if idle
+ */
+bool TWI_bSendData(u8 u8Address, u8 *pData, u8 u8Size);
+
+/**
+ * @brief           Actually kicks off the transmission and sets all the necessary flags at the beginning and puts the slave in read mode.
+ * @param u8Address Target slave address
+ * @param pData     Array pointer for the bytes to be transmitted
+ * @param u8Size    Number of bytes to send
+ * @return true if busy, false if idle
+ */
+ bool TWI_bReadData(u8 u8Address, u8 *pData, u8 u8Size);
+
+/**
+ * @brief Checks if the I2C bus is currently processing a transaction.
+ * @return true if busy, false if idle
+ */
+bool TWI_bIsBusy(void);
+
+/**
+ * @brief Runs on TWINT ISR trigger, handles all TWI events, checks TWSR status register to decide next action.
+ * @note  You can run this function manually if interrupts are disabled.
+ */
+void TWI_vIntHandler(void);
 
 #endif /* TWI_INTERFACE_H_ */
--- TWI/TWI_program.c
+++ TWI/TWI_program.c
@@ -3,3 +3,220 @@
 /* Author:      Youssef Mostafa                                                  */
 /* Description: Low-Level Hardware Manipulation Source Code for ATmega32 TWI.    */
 /*********************************************************************************/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdbool.h>
+#include "TWI_interface.h"
+
+/* Private State Variables for the ISR */
+static u8 *TWI_pDataBuffer;    // Pointer to the user's data array
+static u8 TWI_u8DataSize;      // Total bytes to send/receive
+static u8 TWI_u8DataCounter;   // Current byte index
+static u8 TWI_u8SlaveAddress;  // Target slave address
+static bool TWI_boolDirection; // true = Read, false = Write
+static bool TWI_boolIsBusy;    // Flag to prevent overlapping transactions
+
+/* Set interrupt handler */
+ISR(TWI_vect)
+{
+    TWI_vIntHandler();
+}
+
+void TWI_vInit(u32 u32Freq, bool boolInterruptEnable)
+{
+    /* Set frequency */
+    TWI_vSetFrequency(u32Freq);
+
+    /* Enable TWI peripheral */
+    TWCR |= (1 << TWEN);
+
+    /* Check if user wants to enable interrupts */
+    if (boolInterruptEnable)
+    {
+        TWI_vEnableInterrupt();
+    }
+}
+
+void TWI_vSetFrequency(u32 u32Freq)
+{
+}
+
+void TWI_vSetOwnSlaveAddress(u8 u8Address, bool boolGeneralCall)
+{
+    TWAR = (u8Address << 1) | boolGeneralCall;
+}
+
+void TWI_vAcknowledgeOwnAddress(bool boolAcknowledgeEnable)
+{
+    if (boolAcknowledgeEnable)
+        TWCR |= (1 << TWEA);
+    else
+        TWCR &= ~(1 << TWEA);
+}
+
+void TWI_vEnableInterrupt(void)
+{
+    /* Set TWIE bit to enable TWI interrupts */
+    TWCR |= (1 << TWIE);
+}
+
+void TWI_vWriteBitRateRegister(u8 u8BitRateByte)
+{
+    TWBR = u8BitRateByte;
+}
+
+void TWI_vWritePrescaler(u8 u8Prescaler)
+{
+    if (u8Prescaler <= 3)
+    {
+        TWSR = (TWSR & 0xFC) | u8Prescaler;
+    }
+    else
+    {
+        TWSR = (TWSR & 0xFC) | 3;
+    }
+}
+
+void TWI_vStartTransmission(void)
+{
+    TWCR = (1 << TWSTA) | (1 << TWINT) | (1 << TWEN);
+}
+
+bool TWI_bSendData(u8 u8Address, u8 *pData, u8 u8Size) {
+    if (TWI_boolIsBusy) return false;
+
+    /* Flag and var setup */
+    TWI_u8SlaveAddress = u8Address;
+    TWI_boolDirection  = TWI_WRITE; /* WRITE MODE */
+    TWI_pDataBuffer    = pData;
+    TWI_u8DataSize     = u8Size;
+    TWI_u8DataCounter  = 0;
+    TWI_boolIsBusy     = true;
+
+    /* Send start bit to start transmitting */
+    TWI_vStartTransmission();
+    return true;
+}
+
+bool TWI_bReadData(u8 u8Address, u8 *pData, u8 u8Size) {
+    if (TWI_boolIsBusy) return false;
+
+    /* Flag and var setup */
+    TWI_u8SlaveAddress = u8Address;
+    TWI_boolDirection  = TWI_READ; /* READ MODE */
+    TWI_pDataBuffer    = pData;
+    TWI_u8DataSize     = u8Size;
+    TWI_u8DataCounter  = 0;
+    TWI_boolIsBusy     = true;
+
+    /* Send start bit to start transmitting */
+    TWI_vStartTransmission();
+    return true;
+}
+
+void TWI_vEndTransmission(void)
+{
+    TWCR = (1 << TWSTO) | (1 << TWINT) | (1 << TWEN);
+}
+
+void TWI_vSendSlaveCall(u8 u8Address, bool boolDirectionBit)
+{
+    TWDR = (u8Address << 1) | boolDirectionBit;
+    TWCR = (1 << TWINT) | (1 << TWEN);
+}
+
+void TWI_vSendDataByte(u8 u8DataByte)
+{
+    TWDR = u8DataByte;
+    TWCR = (1 << TWINT) | (1 << TWEN);
+}
+
+u8 TWI_u8ReadDataByte(void)
+{
+    return TWDR;
+}
+
+bool TWI_bIsBusy(void) {
+    return TWI_boolIsBusy;
+}
+
+void TWI_vIntHandler(void)
+{
+    u8 status = TWSR & 0xF8;
+
+    switch (status)
+    {
+    /* ************************************************** */
+    /*             MASTER TRANSMITTER STATES              */
+    /* ************************************************** */
+    case 0x08: /* START TRANSMITTED */
+    case 0x10: /* REPEATED START TRANSMITTED */
+        TWI_vSendSlaveCall(TWI_u8SlaveAddress, TWI_boolDirection);
+        break;
+
+    case 0x18: /* SLA+W TRANSMITTED, ACK RECEIVED */
+    case 0x28: /* DATA BYTE TRANSMITTED, ACK RECEIVED */
+        if (TWI_u8DataCounter < TWI_u8DataSize)
+        {
+            TWI_vSendDataByte(TWI_pDataBuffer[TWI_u8DataCounter]);
+            TWI_u8DataCounter++;
+        }
+        else
+        {
+            TWI_vEndTransmission();
+            TWI_boolIsBusy = false;
+        }
+        break;
+
+    /* ************************************************** */
+    /*             MASTER RECEIVER STATES                 */
+    /* ************************************************** */
+    case 0x40: /* SLA+R TRANSMITTED, ACK RECEIVED */
+        if (TWI_u8DataSize == 1)
+        {
+            TWI_vAcknowledgeOwnAddress(TWI_ACKNOWLEDGE_DISABLE);
+        }
+        else
+        {
+            TWI_vAcknowledgeOwnAddress(TWI_ACKNOWLEDGE_ENABLE);
+        }
+        TWCR = (1 << TWINT) | (1 << TWEN) | (TWCR & (1 << TWEA));
+        break;
+
+    case 0x50: /* DATA BYTE RECEIVED, ACK RETURNED */
+        TWI_pDataBuffer[TWI_u8DataCounter] = TWI_u8ReadDataByte();
+        TWI_u8DataCounter++;
+
+        if (TWI_u8DataCounter == TWI_u8DataSize)
+        {
+            TWI_vEndTransmission();
+            TWI_boolIsBusy = false;
+        }
+        else
+        {
+            if (TWI_u8DataCounter == TWI_u8DataSize - 1)
+            {
+                TWI_vAcknowledgeOwnAddress(TWI_ACKNOWLEDGE_DISABLE);
+            }
+            TWCR = (1 << TWINT) | (1 << TWEN) | (TWCR & (1 << TWEA));
+        }
+
+        /* ************************************************** */
+        /*             ERROR / ARBITRATION STATES             */
+        /* ************************************************** */
+        case 0x38: // Arbitration lost
+        case 0x48: // SLA+R/W transmitted, NACK received (Slave not responding)
+        case 0x20: // SLA+W transmitted, NACK received
+        case 0x30: // Data byte transmitted, NACK received
+            // An error occurred. Abort and send STOP condition.
+            TWI_vEndTransmission();
+            TWI_boolIsBusy = false;
+            break;
+
+        default:
+            // Unknown state. Just clear the flag to prevent hanging.
+            TWCR = (1 << TWINT) | (1 << TWEN);
+            break;
+    }
+}
